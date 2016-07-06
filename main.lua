e7efde96937cd30427547181fa29446c330698ba
Joe Haines <hello@joehaines.co.uk>
2016-07-06 20:59:43 +0100

    Simplify event handling

--- main.lua
+++ main.lua
@@ -1,28 +1,28 @@
-local _, class = UnitClass("player")
+local _, class = UnitClass('player')
 local classColourTable = RAID_CLASS_COLORS[class]
 local colour = classColourTable.colorStr
 local dBefore, hBefore, tBefore
 
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("ADDON_LOADED")
-frame:RegisterEvent("PLAYER_REGEN_ENABLED")
-frame:RegisterEvent("PLAYER_REGEN_DISABLED")
+local frame = CreateFrame('Frame')
+frame:RegisterEvent('ADDON_LOADED')
+frame:RegisterEvent('PLAYER_REGEN_ENABLED')
+frame:RegisterEvent('PLAYER_REGEN_DISABLED')
 
 -- credit to Richard Warburton (http://richard.warburton.it)
 -- via http://lua-users.org/wiki/FormattingNumbers
-local function prettifyNumber(n)
+local function prettifyNumber (n)
   n = math.floor(n + 0.5)
   local left, num, right = string.match(n, '^([^%d]*%d)(%d*)(.-)$')
   return left .. (num:reverse():gsub('(%d%d%d)', '%1,'):reverse()) .. right
 end
 
-local function combatStarted()
+local function combatStarted ()
   dBefore = GetStatistic(197)
   hBefore = GetStatistic(198)
   tBefore = GetTime()
 end
 
-local function combatFinished()
+local function combatFinished ()
   local dAfter = GetStatistic(197)
   local hAfter = GetStatistic(198)
   local tAfter = GetTime()
@@ -36,19 +36,20 @@ local function combatFinished()
   local dps = damage / time
   local hps = healing / time
 
-  print("meep: |c" .. colour .. prettifyNumber(dps) .. "|r DPS |c" .. colour .. prettifyNumber(hps) .. "|r HPS")
+  print('meep: |c' .. colour .. prettifyNumber(dps) .. '|r DPS |c' .. colour .. prettifyNumber(hps) .. '|r HPS')
 end
 
-local function eventHandler(self, event, ...)
-  if event == "ADDON_LOADED" then
-    if ... ~= "meep" then return end
+local eventHandlers = {
+  ADDON_LOADED = function (addon)
+    if addon ~= 'meep' then return end
 
-    print("|c" .. colour .. "meep|r loaded!")
-  elseif event == "PLAYER_REGEN_DISABLED" then
-    combatStarted()
-  else
-    combatFinished()
-  end
-end
+    print('|c' .. colour .. 'meep|r loaded!')
+  end,
 
-frame:SetScript("OnEvent", eventHandler)
+  PLAYER_REGEN_DISABLED = combatStarted,
+  PLAYER_REGEN_ENABLED = combatFinished
+}
+
+frame:SetScript('OnEvent', function (self, event, ...)
+  eventHandlers[event](...)
+end)
